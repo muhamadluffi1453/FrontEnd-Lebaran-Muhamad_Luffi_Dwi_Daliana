0ce03e2d21a275c28a21695163be7eee416e8a63
muhamadluffi1453 <muhamadluffidwidaliana@gmail.com>
2021-09-03 14:44:58 +0700

    FronEnd-Lebaran-Muhamad_Luffi_Dwi_Daliana

---
+++ ketupat.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ketupat Lebaran</title>
+	<style>
+		body{
+			background-color: yellow;
+		}
+		#ketupat {
+			width:200px;
+			height:200px;
+			background:#1eff00;
+			margin:200px 0 0 750px;
+
+			-webkit-transform:rotate(-45deg);
+			-moz-transform:rotate(-45deg);
+			-ms-transform:rotate(-45deg);
+			-o-transform:rotate(-45deg);
+			transform: rotate(-45deg);
+
+			-webkit-transform-origin:0 100%;
+			-moz-transform-origin:0 100%;
+			-ms-transform-origin:0 100%;
+			-o-transform-origin:0 100%;
+			transform-origin: 0 100%;
+		}
+		h1,h3{
+			text-align: center;
+			font-family: sans-serif;
+		}
+	</style>
+</head>
+<body>
+
+	<div id="ketupat"></div>
+		<h1>Selamat Hari Raya Idul Fitri</h1>
+		<h3>Mohon Maaf Lahir dan Batin</h3>
+
+</body>
+</html>
